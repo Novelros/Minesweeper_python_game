679ee61a8132fce78f77e8a9ff96cc1cbb33282e
Aecktan-himera <aecktan-himera@mail.ru>
2025-09-26 15:04:05 +0300

    Update main.py
    Changes in console version:
    1. New function - open around
    2. Iteration instead of recursion

--- game_in_console/main.py
+++ game_in_console/main.py
@@ -68,20 +68,22 @@ def print_map(map, flags=None):
 def get_bomb_count():
     """Метод запроса у пользователя количества мин"""
     while True:
-        count_of_bomb = int(input("Введите одно число - количество мин от 1-80 шт.\n"))
-        if count_of_bomb < 1 or count_of_bomb > 80:
-            continue
-        return count_of_bomb
-        break
+        try: #1. Внесены изменения в проверку введенного количества бомб, сокращен диапазон для улучшения игрового процесса
+            count_of_bomb = int(input("Введите одно число - количество мин от 1-15 шт.\n"))
+            if 1 <= count_of_bomb <= 15:
+                return count_of_bomb
+            print("Число должно быть от 1 до 15!")
+        except ValueError:
+            print("Введите корректное число!")
 
 
 def get_action():
-    """Метод запроса действия: открыть или пометить"""
+    """Метод запроса действия: открыть, пометить или открыть вокруг""" #2. Добавлен функционал - открыть вокруг
     while True:
-        action = input("Выберите действие (d - открыть, f - пометить флагом): ").lower()
-        if action in ('d', 'f'):
+        action = input("Выберите действие (d - открыть, f - пометить флагом, o - открыть вокруг): ").lower()
+        if action in ('d', 'f', 'o'):
             return action
-        print("Используйте 'd' или 'f'!")
+        print("Используйте 'd', 'f' или 'o'!")
 
 
 def get_coordinates():
@@ -104,26 +106,83 @@ def get_coordinates():
 
 
 def reveal_cells(map, player_map, display_map, x, y, size):
-    """Рекурсивно открывает все соседние пустые клетки"""
-    # Проверяем, что координаты в пределах поля и клетка еще не открыта
-    if not (0 <= x < size and 0 <= y < size) or player_map[x][y] != '-':
-        return
+    """Итеративно открывает все соседние пустые клетки"""  #3. Рекурсия заменена на итерации
+    stack = [(x, y)]
+    directions = [(-1, -1), (-1, 0), (-1, 1),
+                  (0, -1), (0, 1),
+                  (1, -1), (1, 0), (1, 1)]
 
-    # Открываем текущую клетку
-    player_map[x][y] = map[x + 1][y + 1]  # +1 из-за заголовков в map
-    display_map[x + 1][y + 1] = player_map[x][y]  # Обновляем отображаемую карту
+    while stack:
+        x, y = stack.pop()
+        if not (0 <= x < size and 0 <= y < size) or player_map[x][y] != '-':
+            continue
 
-    # Если клетка пустая (не число и не мина), открываем соседей
-    if player_map[x][y] == ' ':
-        directions = [(-1, -1), (-1, 0), (-1, 1),
-                      (0, -1), (0, 1),
-                      (1, -1), (1, 0), (1, 1)]
+        player_map[x][y] = map[x + 1][y + 1]
+        display_map[x + 1][y + 1] = player_map[x][y]
 
-        for dx, dy in directions:
-            reveal_cells(map, player_map, display_map, x + dx, y + dy, size)
+        if player_map[x][y] == ' ':
+            for dx, dy in directions:
+                stack.append((x + dx, y + dy))
 
 
-def main():
+def open_around_cell(hidden_map, player_map, display_map, x, y, flags, size):
+    """Открывает все соседние клетки, если количество флагов соответствует цифре""" #4. Новый метод для функцтонала "открыть вокруг"
+    # Проверяем, что клетка открыта и содержит цифру
+    if player_map[x][y] == '-' or player_map[x][y] not in '12345678':
+        print("Эта клетка не содержит цифру или не открыта!")
+        return False
+
+    # Получаем цифру в клетке
+    number = int(player_map[x][y])
+
+    # Подсчитываем количество флагов вокруг
+    directions = [(-1, -1), (-1, 0), (-1, 1),
+                  (0, -1), (0, 1),
+                  (1, -1), (1, 0), (1, 1)]
+
+    flag_count = 0
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < size and 0 <= ny < size and (nx, ny) in flags:
+            flag_count += 1
+
+    # Если количество флагов не соответствует цифре
+    if flag_count != number:
+        print(f"Количество флагов вокруг ({flag_count}) не соответствует цифре в клетке ({number})!")
+        return False
+
+    # Открываем все соседние клетки без флагов
+    mine_hit = False
+    cells_opened = 0
+
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < size and 0 <= ny < size:
+            # Пропускаем клетки с флагами
+            if (nx, ny) in flags:
+                continue
+
+            # Пропускаем уже открытые клетки
+            if player_map[nx][ny] != '-':
+                continue
+
+            # Открываем клетку
+            player_map[nx][ny] = hidden_map[nx + 1][ny + 1]
+            display_map[nx + 1][ny + 1] = player_map[nx][ny]
+            cells_opened += 1
+
+            # Проверяем, не мина ли это
+            if player_map[nx][ny] == 'M':
+                mine_hit = True
+            # Если клетка пустая, открываем соседей рекурсивно
+            elif player_map[nx][ny] == ' ':
+                reveal_cells(hidden_map, player_map, display_map, nx, ny, size)
+
+    print(f"Открыто {cells_opened} клеток вокруг.")
+    return not mine_hit  # Возвращаем False, если нашли мину
+
+
+def main():  #5. Встроен новый метод - "открыть вокруг", переработана логика определения победы
     dictionary_for_x_y = {'y': [], 'x': []}
     bomb_count = get_bomb_count()
 
@@ -146,65 +205,88 @@ def main():
 
     # Обрабатываем первый ход
     reveal_cells(hidden_map, player_map, display_map, first_x, first_y, 9)
-    dictionary_for_x_y['y'].append(first_x + 1)
-    dictionary_for_x_y['x'].append(first_y + 1)
-    print(first_x + 1, first_y + 1, dictionary_for_x_y)
 
     while True:
         print("\nТекущая карта:")
         print_map(display_map, flags)
         action = get_action()
-        x, y = get_coordinates()
 
-        # Проверка на уже открытую клетку
-        if player_map[x][y] != '-':
-            print("Эта клетка уже открыта!")
-            continue
+        if action == 'o':
+            # Для действия "открыть вокруг" клетка должна быть уже открыта
+            x, y = get_coordinates()
+            if player_map[x][y] == '-':
+                print("Сначала откройте эту клетку командой 'd'!")
+                continue
 
-        if action == 'f':  # Пометить флагом
-            if (x, y) in flags:
-                flags.remove((x, y))
-                if hidden_map[x + 1][y + 1] == 'M':
-                    correct_flags.remove((x, y))
-            else:
-                flags.add((x, y))
-                if hidden_map[x + 1][y + 1] == 'M':
-                    correct_flags.add((x, y))
-
-            # Проверка победы по флагам
-            if len(correct_flags) == bomb_count and len(flags) == bomb_count:
-                print("\nПоздравляем! Вы правильно отметили все мины")
+            success = open_around_cell(hidden_map, player_map, display_map, x, y, flags, 9)
+            if not success:
+                print("\nBOOM! Вы наступили на мину")
                 print("Игровое поле:")
-                print_map(hidden_map)
+                # Показываем все мины
+                for i in range(9):
+                    for j in range(9):
+                        if hidden_map[i + 1][j + 1] == 'M':
+                            display_map[i + 1][j + 1] = 'M'
+                print_map(display_map)
                 break
-            continue
+        else:
+            x, y = get_coordinates()
 
-        # Действие: открыть клетку
-        if (x, y) in flags:
-            print("Сначала уберите флаг с этой клетки!")
-            continue
+            # Проверка на уже открытую клетку (кроме действия 'f')
+            if action != 'f' and player_map[x][y] != '-':
+                print("Эта клетка уже открыта!")
+                continue
 
-        # Проверка на мину ДО открытия
-        if hidden_map[x + 1][y + 1] == 'M':
-            print("\nBOOM! Вы наступили на мину")
-            print("Игровое поле:")
-            print_map(hidden_map)
-            break
+            if action == 'f':  # Пометить флагом
+                if (x, y) in flags:
+                    flags.remove((x, y))
+                    if hidden_map[x + 1][y + 1] == 'M':
+                        correct_flags.remove((x, y))
+                else:
+                    flags.add((x, y))
+                    if hidden_map[x + 1][y + 1] == 'M':
+                        correct_flags.add((x, y))
 
-        # Открываем клетку и соседей (если пустая)
-        reveal_cells(hidden_map, player_map, display_map, x, y, 9)
+                # Проверка победы по флагам
+                if len(correct_flags) == bomb_count and len(flags) == bomb_count:
+                    print("\nПоздравляем! Вы правильно отметили все мины")
+                    print("Игровое поле:")
+                    print_map(hidden_map)
+                    break
+                continue
 
-        # Добавляем в историю
-        dictionary_for_x_y['y'].append(x + 1)
-        dictionary_for_x_y['x'].append(y + 1)
-        print(x + 1, y + 1, dictionary_for_x_y)
+            # Действие: открыть клетку
+            if (x, y) in flags:
+                print("Сначала уберите флаг с этой клетки!")
+                continue
 
-        # Проверка победы по открытым клеткам
-        if all(player_map[i][j] != '-' or hidden_map[i + 1][j + 1] == 'M'
-               for i in range(9) for j in range(9)):
+            # Проверка на мину
+            if hidden_map[x + 1][y + 1] == 'M':
+                print("\nBOOM! Вы наступили на мину")
+                print("Игровое поле:")
+                # Показываем все мины
+                for i in range(9):
+                    for j in range(9):
+                        if hidden_map[i + 1][j + 1] == 'M':
+                            display_map[i + 1][j + 1] = 'M'
+                print_map(display_map)
+                break
+
+            # Открываем клетку и соседей (если пустая)
+            reveal_cells(hidden_map, player_map, display_map, x, y, 9)
+
+        # Проверка победы по открытым клеткам (после каждого хода)
+        closed_cells = sum(1 for i in range(9) for j in range(9)
+                           if player_map[i][j] == '-' and hidden_map[i + 1][j + 1] != 'M')
+        if closed_cells == 0:
             print("\nПоздравляем! Вы открыли все безопасные клетки")
             print("Игровое поле:")
-            print_map(hidden_map)
+            # Показываем все мины под флагами
+            for i in range(9):
+                for j in range(9):
+                    if hidden_map[i + 1][j + 1] == 'M':
+                        display_map[i + 1][j + 1] = 'M'
+            print_map(display_map, flags)
             break
 
 
